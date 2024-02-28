8fe4fdb3766113f2270b0cd1ae1ca694e88eed63
Agustina Mastricola <agus.ppye@gmail.com>
2024-02-28 12:44:24 -0300

    Configuracion de colores y tipografias con respecto al figma

--- digitalmoneyhouse/tailwind.config.ts
+++ digitalmoneyhouse/tailwind.config.ts
@@ -7,11 +7,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "total-primary":"#0AEB8C",
+      "total-black": "#151817",
+      "light-primary":"#D2FFEC",
+      "light-black":"#052A2D",
+      "total-white":"#FFFFFF",
+      "total-gray":"#7F7F7F",
+      "medium-gray":"#A6A5A7",
+      "light-gray":"CECECE",
+      "error-text":"#E91010",
+    },
+    fontSize:{
+      
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        "opensans":['Open Sans', 'sans-serif']
       },
     },
   },
