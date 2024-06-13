826adb88a65ac1545a8038e3a05d0fc1a7697cf6
Agus Mastricola <juanz@Juans-MacBook-Pro.local>
2024-06-13 15:12:36 -0300

    Navbar responsive en todos los dispositivos-CORRECCIONES

--- digitalmoneyhouse/src/components/navbar/Navbar.tsx
+++ digitalmoneyhouse/src/components/navbar/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = ({links}:NavbarProps) => {
             priority
           />
         </Link>
-        <ul className='flex w-full space-x-4 col-span-6 col-start-7 items-center sm:col-start-8 md:col-start-9 lg:col-start-10'>
+        <ul className='flex w-full space-x-3 col-span-9 col-start-4 items-center sm:col-start-8 md:col-start-9 lg:col-start-10'>
           {
             links.map((link, index)=>(
               <li key={`navbar-link-${index}`} className="px-3 py-2 border rounded-[5px] text-xs text-total-primary font-bold">
