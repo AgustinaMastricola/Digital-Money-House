7ab6517facca904d1409440e1901eb85fc449ec3
Agus Mastricola <agus.ppye@gmail.com>
2024-07-01 11:18:41 -0300

    Redireccionamiento a la Home luego de login exitoso

--- digitalmoneyhouse/src/services/common/https.service.ts
+++ digitalmoneyhouse/src/services/common/https.service.ts
@@ -8,13 +8,23 @@ export const httpsGet = async <T>(endpoint:string, params?:URLSearchParams): Pro
     }
     return res.json()
 }
-export const httpsPost = async <T>(endpoint:string, body: object): Promise<T> => {
+export const httpsPost = async <T>(endpoint:string, body: object, skipAuthorization?: boolean): Promise<T> => {
     const res = await fetch(`${URL_BASE}${endpoint}`,{
         method: 'POST',
-        headers:{
-            'Content-Type':'application/json',
-        },
+        headers: 
+            skipAuthorization ? 
+            {   
+                'Content-Type':'application/json',
+            }
+            :
+            {
+                'Content-Type':'application/json',
+                'Authorization':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjI4OSIsImVtYWlsIjoiYWd1c21hc3RyaWNAZ21haWwuY29tIiwiZXhwIjoxNzE5NTI4OTcyfQ.pwrLHI3ingEoZ_N2vnrFkDmD3jTElQW5plGGbmGQWOc'
+            },
         body: JSON.stringify(body)
     })
+    if (res.status===200){
+        window.location.replace('/')
+    }
     return res.json()
 }
