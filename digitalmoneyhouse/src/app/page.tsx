fcf0856fe3fa373183b1f041ca874e4e876a4092
Agustina Mastricola <agus.ppye@gmail.com>
2024-08-16 11:49:58 -0300

    Correccion de semantica pagina Home

--- digitalmoneyhouse/src/app/page.tsx
+++ digitalmoneyhouse/src/app/page.tsx
@@ -3,18 +3,11 @@ import Footer from "@/components/common/footer/Footer";
 import HomeAnauthenticated from "@/components/home/anauthenticated/HomeAnauthenticated";
 import HomeAuthenticated from "@/components/home/authenticated/HomeAuthenticated";
 import { useSession } from "next-auth/react";
-import Header from "@/components/common/navbar/Header";
 
-const links = [
-  {href:"/login", name:"Ingresar", outline: true},
-  {href:"/signup", name: "Crear cuenta"},
-] 
 export default function Home() {
   const { data: session, status } = useSession()
   return (
     <>
-      
-      <main className="h-full">
         {status === 'authenticated'?
           <HomeAuthenticated/>
         :
@@ -23,7 +16,6 @@ export default function Home() {
         :  
           <HomeAnauthenticated/>
         }
-      </main>
       <Footer styleContainer="bg-total-black" styleParagraph="text-total-primary"/>
     </>
   );
