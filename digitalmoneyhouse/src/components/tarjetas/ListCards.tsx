57f3003a23adad86cb19b88cfc8b2694abff7ec6
Agustina Mastricola <agus.ppye@gmail.com>
2024-08-26 14:09:15 -0300

    Conecto el boton Eliminar tarjeta, con el endpoint

--- digitalmoneyhouse/src/components/tarjetas/ListCards.tsx
+++ digitalmoneyhouse/src/components/tarjetas/ListCards.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import ElipseIcon from "@/components/common/icons/ElipseIcon"
+import { CardType } from "@/types/card.types"
+import Button from "../common/buttons/Button"
+import cardsAPI from "@/services/cards/cards.service"
+import { useSession } from "next-auth/react"
+import accountAPI from "@/services/account/account.service"
 
-const ListCards = () => {
+type ListCardsProps = {
+  cardsList: CardType[]
+}
+
+const ListCards = async ({cardsList}:ListCardsProps) => {
+  const {data: session, status} = useSession();
+  const token = session?.user.token;
+  const getAccount = await accountAPI.getMyAccount(token)
+
+  const deleteCard = async (card_id:number) => {
+    const res = await cardsAPI.deleteCard(token, getAccount.id, card_id)
+  }
+  
   return (
-    <div>ListCards</div>
+    <div className="w-11/12 pl-2 pt-3 md:pl-10 lg:pl-4 flex flex-col items-start border border-total-gray border-opacity-15 rounded-lg border-1  bg-total-white drop-shadow-2xl ">
+      <h1 className="text-base my-2">Tus tarjetas</h1>
+      <div className="w-11/12 flex flex-col-reverse">
+        { cardsList ?
+          cardsList.map((item, index)=>(
+            <div key={`card-${index}`}>
+              <hr className="text-medium-gray opacity-30"/>
+              <div  className="grid gap-x-2 grid-cols-12 items-center my-3 w-full text-sm md:text-base">
+                <ElipseIcon className={"fill-total-primary"}/>
+                <p className="col-span-6 ml-2">Terminada en {item.number_id.toLocaleString().replace(/\D/g, '').slice(-4)}</p>
+                <div className="flex flex-col col-span-5 items-start col-start-9 lg:col-start-11 xl:col-start-12">
+                  <Button children={'Eliminar'} className="border-none" onClick={()=>deleteCard(item.id)} />
+                </div>
+              </div>
+            </div>
+          )) 
+          :
+          <>
+          <p>No tienes tarjetas agregadas en tu cuenta</p>
+          </>
+        }
+      </div>
+    </div>
   )
 }
 
