bb76dd1da924a1ed8feddf07b0f078f9c089884c
Agustina Mastricola <agus.ppye@gmail.com>
2024-08-26 12:07:20 -0300

    Implementamos un layout para el dashboard, y eliminamos repeticion de codigo en las paginas del dashboard

--- digitalmoneyhouse/src/app/dashboard/servicios/page.tsx
+++ digitalmoneyhouse/src/app/dashboard/servicios/page.tsx
@@ -1,24 +1,9 @@
 'use client'
 
-import Footer from "@/components/common/footer/Footer"
-import Menu from "@/components/common/menu/Menu"
-import HeaderDashboard from "@/components/common/navbar/HeaderDashboard"
-
 const page = () => {
   return (
 		<>
-			<HeaderDashboard/>
-			<main>
-				<div className="md:w-full md:flex md:justify-between">
-					<div className="hidden md:block w-4/12 lg:w-3/12 xl:w-2/12">
-						<Menu />
-					</div>
-					<div className="flex flex-col space-y-4 items-center py-5 md:w-8/12 lg:w-9/12 xl:w-10/12">
-						SERVICIOS
-					</div>
-				</div>
-			</main>
-			<Footer styleContainer="bg-total-black" styleParagraph="text-total-primary"/>
+			SERVICIOS
 		</>
   )
 }
