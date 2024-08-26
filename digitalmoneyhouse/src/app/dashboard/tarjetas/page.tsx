6214a934401fb5ce3ad250ef111898a0eb7fd5e4
Agustina Mastricola <agus.ppye@gmail.com>
2024-08-26 13:29:32 -0300

    Se creao pagina tarjetas con componente superior

--- digitalmoneyhouse/src/app/dashboard/tarjetas/page.tsx
+++ digitalmoneyhouse/src/app/dashboard/tarjetas/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import ArrowRightIcon from "@/components/common/icons/ArrowRight";
+import AddCard from "@/components/tarjetas/AddCard";
+import ListCards from "@/components/tarjetas/ListCards";
+
 const page = () => {
 	return (
 		<>
-			TARJETAS
+			<div className="flex space-x-2 text-sm w-11/12 items-center md:hidden">
+        <ArrowRightIcon/>
+        <span className="underline">Tarjetas</span>
+      </div>
+			<AddCard/>
+			<ListCards/>
 		</>
 	);
 };
