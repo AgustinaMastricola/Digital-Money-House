118f5b661ae0a8ddaecaa5f605e95ff9e8ebbd17
Agustina Mastricola <agus.ppye@gmail.com>
2024-08-29 17:12:47 -0300

    editanto dashboard

--- digitalmoneyhouse/src/components/home/authenticated/ActivityList.tsx
+++ digitalmoneyhouse/src/components/home/authenticated/ActivityList.tsx
@@ -26,7 +26,7 @@ const ActivityList = ({transactions}:ActivityListProps) => {
             <div key={`tansaction-${index}`}>
               <hr className="text-medium-gray opacity-30"/>
               <div  className="grid gap-x-2 grid-cols-12 items-center my-3 w-full text-sm md:text-base">
-                <ElipseIcon className={"fill-total-primary"}/>
+                <ElipseIcon className={"fill-total-primary"} width="18" height="18"/>
                 <p className="col-span-6 ml-2">{item.description}</p>
                 <div className="flex flex-col col-span-5 items-start col-start-9 lg:col-start-11 xl:col-start-12">
                   <p>{item.description.match('transfriste')? `$ ${item.amount}`: `$ ${item.amount.toLocaleString('de-DE')}` }</p>
