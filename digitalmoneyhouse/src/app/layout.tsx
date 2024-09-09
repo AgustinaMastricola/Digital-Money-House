c2ef3fcf3b1ddc7c39aa39c7921c9bfd8450cc1f
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-09 10:55:40 -0300

    Se ustiliza el conexto en el componente UserName usando en el componente HeaderDashboard, para probar que funcione correctamente y asi poder cumplir con las especificaciones de los componentes de cliente

--- digitalmoneyhouse/src/app/layout.tsx
+++ digitalmoneyhouse/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import SessionAuthProvider from "../../context/SessionAuthProvider";
+import SessionAuthProvider from "../context/SessionAuthProvider";
+import UserProvider from "@/context/UserContextProvider";
 
 const openSans = Open_Sans({subsets: ["latin"], style: ['normal']});
 
@@ -15,7 +16,9 @@ export default function RootLayout( {children}: Readonly<{children: React.ReactN
     <html lang="en">
       <body className="flex flex-col `${openSans.className}`">
         <SessionAuthProvider>
-          {children}
+          <UserProvider>
+            {children}
+          </UserProvider>
         </SessionAuthProvider>
       </body>
     </html>
--- digitalmoneyhouse/src/components/common/navbar/UserName.tsx
+++ digitalmoneyhouse/src/components/common/navbar/UserName.tsx
@@ -1,33 +1,21 @@
-import accountAPI from "@/services/account/account.service";
-import userApi from "@/services/users/user.service";
-import { useSession } from "next-auth/react";
+
+import { useUserContext } from "@/context/UserContextProvider";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const UserName = () => {
-	const { data: session, status } = useSession();
 	const [initalsName, setInitialsName] = useState("");
-  const [firstname, setFirstname] = useState("");
-	const [lastname, setLastname] = useState("");
+	const {firstname,lastname,token,user_id,account_id} = useUserContext()
 
-	const getDataUser = async () => {
-		if (session?.user?.token) {
-			const getAccount = await accountAPI.getMyAccount(`${session.user.token}`);
-			const res = await userApi.getUserData(
-				`${session.user.token}`,
-				getAccount.user_id
-			);
-			console.log(res);
-			const f = res.firstname.charAt(0).toUpperCase();
-			const l = res.lastname.charAt(0).toUpperCase();
+	function getInitialsName() {
+			const f = firstname.charAt(0).toUpperCase();
+			const l = lastname.charAt(0).toUpperCase();
 			setInitialsName(f + l);
-      setFirstname(res.firstname);
-			setLastname(res.lastname);
 		}
-	};
+	
 	useEffect(() => {
-		getDataUser();
-	}, [session?.user]);
+		getInitialsName();
+	}, [firstname]);
 
 	return (
 		<div className="flex items-center space-x-3">
@@ -47,3 +35,7 @@ const UserName = () => {
 };
 
 export default UserName;
+function getInitialsName() {
+	throw new Error("Function not implemented.");
+}
+
