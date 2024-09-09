4232c10671349ec682fafe71070aea5a557376d0
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-09 12:22:08 -0300

    Refactorizacion: refactor del componente UserName

--- digitalmoneyhouse/src/components/common/navbar/UserName.tsx
+++ digitalmoneyhouse/src/components/common/navbar/UserName.tsx
@@ -1,21 +1,14 @@
-
 import { useUserContext } from "@/context/UserContextProvider";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 const UserName = () => {
-	const [initalsName, setInitialsName] = useState("");
-	const {firstname,lastname,token,user_id,account_id} = useUserContext()
+	const { firstname, lastname } = useUserContext();
 
-	function getInitialsName() {
-			const f = firstname.charAt(0).toUpperCase();
-			const l = lastname.charAt(0).toUpperCase();
-			setInitialsName(f + l);
-		}
-	
-	useEffect(() => {
-		getInitialsName();
-	}, [firstname]);
+	function getInitialsName(firstname: string, lastname: string) {
+		const f = firstname.charAt(0).toUpperCase();
+		const l = lastname.charAt(0).toUpperCase();
+		return `${f}${l}`;
+	}
 
 	return (
 		<div className="flex items-center space-x-3">
@@ -23,19 +16,15 @@ const UserName = () => {
 				className="text-total-black bg-total-primary uppercase p-[0.4rem] rounded-lg font-bold md:text-lg"
 				href={"/dashboard/perfil"}
 			>
-				{initalsName}
+				{getInitialsName(firstname, lastname)}
 			</Link>
-			<Link 
-        className="hidden md:items-center md:space-x-2 md:text-total-primary md:pr-5 md:flex"
-        href={"/dashboard/perfil"} >
-					Hola, {firstname} {lastname}
+			<Link
+				className="hidden md:items-center md:space-x-2 md:text-total-primary md:pr-5 md:flex"
+				href={"/dashboard/perfil"}
+			>
+				Hola, {firstname} {lastname}
 			</Link>
 		</div>
 	);
 };
-
 export default UserName;
-function getInitialsName() {
-	throw new Error("Function not implemented.");
-}
-
