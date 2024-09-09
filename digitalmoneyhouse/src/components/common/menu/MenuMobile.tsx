f528928346083fdbb4943890463275a9cdd40e32
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-09 11:12:09 -0300

    Refactorizacion: se utiliza UserContext en componente MenuMobile, para obtener nombre y apellido del usuario

--- digitalmoneyhouse/src/components/common/menu/MenuMobile.tsx
+++ digitalmoneyhouse/src/components/common/menu/MenuMobile.tsx
@@ -1,33 +1,12 @@
-import { useSession } from "next-auth/react";
 import Menu from "./Menu"
-import { useEffect, useState } from "react";
-import accountAPI from "@/services/account/account.service";
-import userApi from "@/services/users/user.service";
+import { useUserContext } from "@/context/UserContextProvider";
 
 type MenuMobileProps= {
 	onClickLink?: () => void 
 }
 
 const MenuMobile = ({ onClickLink }: MenuMobileProps) => {
-	const { data: session, status } = useSession();
-	const [firstname, setFirstname] = useState("");
-	const [lastname, setLastname] = useState("");
-	
-	const getDataUser = async () => {
-		if (session?.user?.token) {
-			const getAccount = await accountAPI.getMyAccount(`${session.user.token}`);
-			const res = await userApi.getUserData(
-				`${session.user.token}`,
-				getAccount.user_id
-			);
-			console.log(res);
-			setFirstname(res.firstname);
-			setLastname(res.lastname);
-		}
-	};
-	useEffect(() => {
-		getDataUser();
-	}, [session?.user?.email]);
+	const {firstname,lastname,token,user_id,account_id} = useUserContext()
 
 	return (
 		<div className="bg-total-primary z-10 h-screen absolute right-0 top-0 w-3/5 md:hidden">
