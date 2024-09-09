5b5a8d7842a4bedb5ab16593da69b80efceac5ce
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-09 15:09:45 -0300

    Refactor: Se elimino la logica de obtencion de datos de la page, trasladada al componente que utiliza dicha informacion, utilizando el nuevo Hook useAccountContext

--- digitalmoneyhouse/src/app/dashboard/page.tsx
+++ digitalmoneyhouse/src/app/dashboard/page.tsx
@@ -1,26 +1,17 @@
-"use client";
 import ActivityList from "@/components/home/authenticated/ActivityList";
 import CardUser from "@/components/home/authenticated/CardUser";
-import { useSession } from "next-auth/react";
-import accountAPI from "@/services/account/account.service";
-import transactionsAPI from "@/services/transactions/transactions.service";
 import SearchIcon from "@/components/common/icons/SearchIcon";
 import ArrowRightIcon from "@/components/common/icons/ArrowRight";
 import Button from "@/components/common/buttons/Button";
 
-export default async function DashboardPage() {
-	const {data: session, status} = useSession();
-  const token = session?.user.token;
-  const getAccount = await accountAPI.getMyAccount(token)
-  const activitiesList = await transactionsAPI.getAllTransactionsUser(token, getAccount.id)
-
+export default function DashboardPage() {
   return (
     <>
       <div className="flex space-x-2 text-sm w-11/12 items-center md:hidden my-2">
         <ArrowRightIcon/>
         <span className="underline">Inicio</span>
       </div>
-      <CardUser amount={`${getAccount.available_amount}`}/>
+      <CardUser/>
       <div className="flex flex-col items-center space-y-4 w-full lg:flex-row lg:items-center lg:space-x-4 lg:w-11/12">
         <Button children={"Ingresar dinero"} asLink={true} href={"/transacciones"} className="bg-total-primary border border-total-primary text-total-black w-11/12 lg:w-10/12 text-center py-4 md:py-5 lg:py-6 font-bold rounded-lg md:h-18 lg:text-xl mt-4"/>
         <Button children={"Pago de servicios"} asLink={true} href={"/servicios"} className="bg-total-primary border border-total-primary text-total-black w-11/12 lg:w-10/12 text-center py-4 md:py-5 lg:py-6 font-bold rounded-lg md:h-18 lg:text-xl"/>
@@ -31,7 +22,7 @@ export default async function DashboardPage() {
           <input placeholder="Buscar en tu actividad" className="hide-arrow p-3 pl-10 w-full border-t border-total-gray border-opacity-15 rounded-lg border-t-1  bg-total-white shadow-lg focus:outline-none"/>
         </div>
       </div>
-      <ActivityList transactions={activitiesList}/>
+      {/* <ActivityList/> */}
     </>
   )
 }
