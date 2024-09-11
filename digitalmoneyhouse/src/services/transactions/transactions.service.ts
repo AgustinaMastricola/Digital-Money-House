0b9b270663dd3e73dcd89a8ca89f82161f0418a6
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-11 16:07:18 -0300

    Se agregan logs para ver cuantas veces se ejecutan los contextos

--- digitalmoneyhouse/src/services/transactions/transactions.service.ts
+++ digitalmoneyhouse/src/services/transactions/transactions.service.ts
@@ -1,6 +1,6 @@
 import { TransferenceType } from "@/types/transference.types";
 class TransactionsAPI {
-  getAllTransactionsUser = async (token: string | null, account_id: number | null):Promise<TransferenceType[]> =>  {
+  getAllTransactionsUser = async (token: string, account_id: number):Promise<TransferenceType[]> =>  {
     const res = await fetch(`https://digitalmoney.digitalhouse.com/api/accounts/${account_id}/activity`, {
       method: 'GET',
       headers: {
