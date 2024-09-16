9ba004ad74e49ede31e042462c46cf4c5fec3b1b
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-16 15:48:32 -0300

    Se modifico el transaction.service para consumir el nuevo endpoint de Next

--- digitalmoneyhouse/src/services/transactions/transactions.service.ts
+++ digitalmoneyhouse/src/services/transactions/transactions.service.ts
@@ -1,16 +1,13 @@
 import { TransferenceType } from "@/types/transference.types";
 class TransactionsAPI {
-  getAllTransactionsUser = async (token: string, account_id: number):Promise<TransferenceType[]> =>  {
-    const res = await fetch(`https://digitalmoney.digitalhouse.com/api/accounts/${account_id}/activity`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization : `${token}`
-      },
-    })
-    if (!res.ok) {
-      console.log('error al obtener las transacciones')
-    }
+  getAllTransactionsUser = async (account_id: number, token: string):Promise<TransferenceType[]> =>  {
+    const res = await fetch(`http://localhost:3000/api/account/${account_id}/activity`,
+      {
+        headers:{
+          'Content-Type': 'application/json',
+          Authorization: token
+        }
+      })
     return res.json();
   }
   getASpecificTransactionUser = async (token: string, account_id: number, transaction_id: number ):Promise<TransferenceType[]> =>  {
