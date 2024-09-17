f9ffbd862272f9f9a94ff1f8d0a6e2a5b7dcc3f5
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-17 11:29:03 -0300

    Implementamos el contexto para consumir el monto disponible

--- digitalmoneyhouse/src/components/home/authenticated/AvailableAmount.tsx
+++ digitalmoneyhouse/src/components/home/authenticated/AvailableAmount.tsx
@@ -1,19 +1,8 @@
 'use client'
-import accountAPI from '@/services/account/account.service';
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react'
+import { useAccountContext } from '@/context/AccountContextProvider';
 
 const AvailableAmount = () => {
-  const {data: session} = useSession();
-  const [amount, setAmount] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await accountAPI.getMyAccount(`${session?.user.token}`);
-      setAmount(data.available_amount);
-    };
-    fetchData();
-  }, [session?.user.token]); 
+  const {available_amount} = useAccountContext();
 
   function formatAmount(amount: string){
     return `$ ${amount.replace(/\B(?=(\d{3})+(?!\d))/g, ".")},00`
@@ -21,7 +10,7 @@ const AvailableAmount = () => {
 
   return (
     <p className='border rounded-full border-total-primary text-center px-8 py-2 text-xl'>
-      {formatAmount(`${amount}`)}
+      {formatAmount(`${available_amount}`)}
     </p>
   )
 }
