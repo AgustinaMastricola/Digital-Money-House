eefbd9e656eba0232960f38b924ef3f8358d36ad
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-17 11:29:43 -0300

    Correccion de la URL

--- digitalmoneyhouse/src/services/account/account.service.ts
+++ digitalmoneyhouse/src/services/account/account.service.ts
@@ -2,7 +2,7 @@ import { AccountData } from "@/types/account.types";
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 class AccountAPI {
   getMyAccount = async (token: string):Promise<AccountData> => {
-    const res = await fetch(`${API_URL}/account`, {
+    const res = await fetch(`${API_URL}account`, {
       headers: {
         Authorization : token
       },
