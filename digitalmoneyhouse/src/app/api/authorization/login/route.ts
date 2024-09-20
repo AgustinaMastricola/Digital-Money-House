df78d75fc33d51f8569485dd8b21df9e8b43f32a
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-20 14:17:52 -0300

    Refactoizacion de los endpoints

--- digitalmoneyhouse/src/app/api/authorization/login/route.ts
+++ digitalmoneyhouse/src/app/api/authorization/login/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-
+const API_BACK_URL = process.env.NEXT_PUBLIC_API_BACK_END
 //Este endpoint tiene un POST para el inicio de sesion del usuario
 export const POST = async (req: NextRequest) => {
   const loginData = await req.json();
   //const data: SignupFormData = await req.json(); --- esta es la forma de tipar para Typescript
 
   try{
-    const results = await fetch(`https://digitalmoney.digitalhouse.com/api/login`,{ 
+    const results = await fetch(`${API_BACK_URL}api/login`,{ 
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
