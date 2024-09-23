9aefd4fcc181ae65f653e8d6aa708490881419aa
Agustina Mastricola <agus.ppye@gmail.com>
2024-09-23 13:31:32 -0300

    Se modifica funcion transformadora de fecha, se crean componentes de pagina Actividad

--- digitalmoneyhouse/src/app/dashboard/actividad/page.tsx
+++ digitalmoneyhouse/src/app/dashboard/actividad/page.tsx
@@ -1,9 +1,22 @@
 'use client'
+import ActivityListFilter from "@/components/actividad/ActivityListFilter"
+import ArrowRightIcon from "@/components/common/icons/ArrowRight"
+import SearchIcon from "@/components/common/icons/SearchIcon"
 
 const ActivityPage = () => {
   return (
 		<>
-			ACTIVIDAD
+			<div className="flex space-x-2 text-sm w-10/12 items-center md:hidden my-2">
+        <ArrowRightIcon className="#000000"/>
+        <span className="underline">Tu actividad</span>
+      </div>
+			<div className=" w-10/12">
+        <div className="w-full flex items-center relative mb-4">
+          <SearchIcon className="h-min absolute left-2"/>
+          <input placeholder="Buscar en tu actividad" className="hide-arrow p-3 pl-10 w-full border-t border-total-gray border-opacity-15 rounded-lg border-t-1  bg-total-white shadow-lg focus:outline-none"/>
+        </div>
+      </div>
+			<ActivityListFilter/>
 		</>
   )
 }
--- digitalmoneyhouse/src/components/home/authenticated/ActivityList.tsx
+++ digitalmoneyhouse/src/components/home/authenticated/ActivityList.tsx
@@ -5,35 +5,65 @@ import { useUserContext } from "@/context/UserContextProvider";
 import transactionsAPI from "@/services/transactions/transactions.service"
 import { TransferenceType } from "@/types/transference.types";
 import transformDay from "@/utils/functions"
+import { usePathname } from "next/navigation";
 import { memo, useEffect, useState } from "react"
 
 const ActivityList = () => {
   const {token} = useUserContext()
   const {id} = useAccountContext() 
   const [activityList, setActivityList] = useState<TransferenceType[]>([]);
+  const location = usePathname();
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await transactionsAPI.getAllTransactionsUser(id, token);
       setActivityList(data);
     };
-
     fetchData();
   }, [token, id]);
+
+  const transformDate = (date: string) => {
+    const dateTransaction = new Date(date);
+    const dateNow = new Date();
+    const dayNow = dateNow.getDate();
+    const dayTransaction = dateTransaction.getDate();
+    const monthNow = dateNow.getMonth();
+    const monthTransaction = dateTransaction.getMonth();
+    const difDays = dayNow - dayTransaction;
   
-  const transformDate = (date:string) => {
-    const fecha = new Date(date)
-    const dayName = fecha.getDay()
-    const day = transformDay(dayName)
-    return day
-  }  
-  const lastTenResults = Array.isArray(activityList) ? activityList.slice(0, 10) : []
+    // Verificar si la diferencia de días es mayor a 7 o menor a -7, o si son de meses diferentes
+    if (difDays > 7 || difDays < -7 || monthNow !== monthTransaction) {
+      const dayName = transformDay(dateTransaction.getDay());
+      const day = dateTransaction.getDate();
+      const month = dateTransaction.toLocaleString('default', { month: 'short' });
+      return `${dayName} ${day} ${month}`;
+    } else {
+      const dayName = transformDay(dateTransaction.getDay());
+      return dayName;
+    }
+  }
+
+  const getFilteredActivityList = () => {
+    // Ordenar el array por fecha en orden descendente
+    if(!Array.isArray(activityList)) {
+      return [];
+    }
+    const sortedActivityList = [...activityList].sort((a, b) => new Date(a.dated).getTime() - new Date(b.dated).getTime());
+
+    if(location === '/dashboard'){
+      // Tomar los primeros 10 elementos del array ordenado
+      return sortedActivityList.slice(-10);
+    }
+    return sortedActivityList;
+  }
+
+  const filteredActivityList = getFilteredActivityList();
 
   return (
     <div className="w-11/12 flex flex-col-reverse">
-        { lastTenResults.length > 0 ?
-          lastTenResults.map((item, index)=>(
-            <div key={`tansaction-${index}`}>
+        { filteredActivityList.length > 0 ?
+          filteredActivityList.map((item, index)=>( 
+            <div key={`transaction-${index}`}>
               <hr className="text-medium-gray opacity-30"/>
               <div  className="grid gap-x-2 grid-cols-12 items-center my-3 w-full text-sm md:text-base">
                 <ElipseIcon className={"fill-total-primary"} width="18" height="18"/>
@@ -46,9 +76,7 @@ const ActivityList = () => {
             </div>
           )) 
           :
-          <>
           <p>No tienes transacciones en tu cuenta</p>
-          </>
         }
       </div>
   )
