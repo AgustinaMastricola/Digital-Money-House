3da3c6e9cfb890b0eff62e85cef61996edddd385
Agustina Mastricola <agus.ppye@gmail.com>
2024-10-30 10:16:29 -0300

    Correccion: Al hacer click en el nombre, lleva a la pagina de perfil en vez de al Dashboard

--- digitalmoneyhouse/src/components/common/navbar/UserName.tsx
+++ digitalmoneyhouse/src/components/common/navbar/UserName.tsx
@@ -15,13 +15,13 @@ const UserName = () => {
 		<div className="flex items-center space-x-3">
 			<Link
 				className="text-total-black bg-total-primary uppercase p-[0.4rem] rounded-lg font-bold md:text-lg"
-				href={"/dashboard/perfil"}
+				href={"/dashboard"}
 			>
 				{initials}
 			</Link>
 			<Link
 				className="hidden md:items-center md:space-x-2 md:text-total-primary md:pr-5 md:flex"
-				href={"/dashboard/perfil"}
+				href={"/dashboard"}
 			>
 				Hola, {firstname} {lastname}
 			</Link>
